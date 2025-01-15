7dff9287c5d43a671b328b5c9c870eadfc1502e4
Aleksander Lantsov <lantsov.aleksander@gmail.com>
2025-01-15 12:15:28 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -6,15 +6,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint:depguard
 )
 
 const (
 	sleepPerStage = time.Millisecond * 100
 	fault         = sleepPerStage / 2
 )
-
-var isFullTesting = true
 
 func TestPipeline(t *testing.T) {
 	// Stage generator
@@ -64,7 +62,7 @@ func TestPipeline(t *testing.T) {
 			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
 	})
 
-	t.Run("done case", func(t *testing.T) {
+	t.Run("termination case", func(t *testing.T) {
 		in := make(Bi)
 		done := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
@@ -96,9 +94,6 @@ func TestPipeline(t *testing.T) {
 }
 
 func TestAllStageStop(t *testing.T) {
-	if !isFullTesting {
-		return
-	}
 	wg := sync.WaitGroup{}
 	// Stage generator
 	g := func(_ string, f func(v interface{}) interface{}) Stage {
@@ -124,7 +119,7 @@ func TestAllStageStop(t *testing.T) {
 		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
 	}
 
-	t.Run("done case", func(t *testing.T) {
+	t.Run("termination case#1 - closing done channel", func(t *testing.T) {
 		in := make(Bi)
 		done := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
@@ -150,6 +145,115 @@ func TestAllStageStop(t *testing.T) {
 		wg.Wait()
 
 		require.Len(t, result, 0)
+	})
 
+	t.Run("termination case#2 - sending into done channel", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		// Abort after 200ms
+		abortDur := sleepPerStage * 2
+		go func() {
+			<-time.After(abortDur)
+			done <- struct{}{}
+		}()
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		wg.Wait()
+
+		require.Len(t, result, 0)
+	})
+
+	t.Run("termination case#3 - slow pipeline consumer", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		// Abort after 500ms - to be sure that first pipeline out msg will be completed
+		abortDur := sleepPerStage * 5
+		go func() {
+			<-time.After(abortDur)
+			done <- struct{}{}
+		}()
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		out := ExecutePipeline(in, done, stages...)
+		time.Sleep(10 * sleepPerStage)
+		wg.Wait()
+
+		_, ok := <-out
+		require.False(t, ok)
+	})
+}
+
+func TestPanicInStage(t *testing.T) {
+	done := make(Bi)
+	wg := sync.WaitGroup{}
+	// Stage generator
+	g := func(_ string, f func(v interface{}) interface{}) Stage {
+		return func(in In) Out {
+			out := make(Bi)
+			wg.Add(1)
+			go func() {
+				defer func() {
+					if r := recover(); r != nil {
+						done <- r
+					}
+				}()
+				defer wg.Done()
+				defer close(out)
+				for v := range in {
+					out <- f(v)
+				}
+			}()
+			return out
+		}
+	}
+
+	stages := []Stage{
+		g("Dummy", func(v interface{}) interface{} { return v }),
+		g("Divider (42 / x )", func(v interface{}) interface{} { return 42 / v.(int) }),
+		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
+		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+	}
+
+	t.Run("panic case", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{3, 2, 1, 0, 10, 11, 12}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		wg.Wait()
+
+		require.Less(t, len(result), 4)
+		for idx, msg := range result {
+			require.Equal(t, msg, []string{"114", "121", "142"}[idx])
+		}
 	})
 }
