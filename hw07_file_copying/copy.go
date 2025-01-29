fd4e537810e691b2690374b3e945608b1e2c866e
Aleksander Lantsov <lantsov.aleksander@gmail.com>
2025-01-29 22:28:20 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/cheggaaa/pb/v3" //nolint:depguard
 )
 
 var (
@@ -10,6 +16,60 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	inputFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("error opening file to read from: %w", err)
+	}
+	defer SafeCloser{inputFile, "file to read from"}.SafeClose()
+
+	fi, err := os.Stat(fromPath)
+	if err != nil {
+		return fmt.Errorf("error getting file to read from info: %w", err)
+	}
+
+	if !fi.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	size := fi.Size()
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if maxLimit := size - offset; limit == 0 || limit >= maxLimit {
+		limit = maxLimit
+	}
+
+	outputFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("error creating file to write to: %w", err)
+	}
+	defer SafeCloser{outputFile, "file to write to"}.SafeClose()
+
+	if _, err = inputFile.Seek(offset, io.SeekStart); err != nil {
+		return fmt.Errorf("error seeking file to read from: %w", err)
+	}
+
+	bar := pb.Start64(limit)
+	defer bar.Finish()
+
+	reader := bar.NewProxyReader(io.LimitReader(inputFile, limit))
+
+	if _, err = io.Copy(outputFile, reader); err != nil {
+		return fmt.Errorf("error copying data: %w", err)
+	}
+
 	return nil
+}
+
+type SafeCloser struct {
+	io.Closer
+	name string
+}
+
+func (sf SafeCloser) SafeClose() {
+	err := sf.Close()
+	if err != nil {
+		log.Printf("error closing %s: %v", sf.name, err)
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,96 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"errors"
+	"math"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+func TestCopyEntireFile(t *testing.T) {
+	fromFile := filepath.Join("testdata", "input.txt")
+	toFile := filepath.Join(t.TempDir(), "copy.txt")
+
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(toFile)
+
+	if err := Copy(fromFile, toFile, 0, 0); err != nil {
+		t.Fatal(err)
+	}
+
+	fromData, err := os.ReadFile(fromFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	toData, err := os.ReadFile(toFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(fromData, toData) {
+		t.Error("Files do not match")
+	}
+}
+
+func TestCopyWithOffsetAndLimit(t *testing.T) {
+	fromFile := filepath.Join("testdata", "input.txt")
+	toFile := filepath.Join(t.TempDir(), "copy.txt")
+
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(toFile)
+
+	if err := Copy(fromFile, toFile, 10, 110); err != nil {
+		t.Fatal(err)
+	}
+
+	fromData, err := os.ReadFile(fromFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	toData, err := os.ReadFile(toFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(fromData[10:120], toData) {
+		t.Error("Files do not match")
+	}
+}
+
+func TestOffsetExceedsFileSize(t *testing.T) {
+	fromFile := filepath.Join("testdata", "input.txt")
+	toFile := filepath.Join(t.TempDir(), "copy.txt")
+
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(toFile)
+
+	err := Copy(fromFile, toFile, math.MaxInt64, 110)
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+	if !errors.Is(err, ErrOffsetExceedsFileSize) {
+		t.Errorf("Expected error %q, got %q", ErrOffsetExceedsFileSize, err.Error())
+	}
+}
+
+func TestNonRegularFile(t *testing.T) {
+	dir := t.TempDir()
+	toFile := filepath.Join(t.TempDir(), "copy.txt")
+
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(toFile)
+
+	err := Copy(dir, toFile, math.MaxInt64, 110)
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+	if !errors.Is(err, ErrUnsupportedFile) {
+		t.Errorf("Expected error %q, got %q", ErrUnsupportedFile, err.Error())
+	}
 }
