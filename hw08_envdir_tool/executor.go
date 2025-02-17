1a1efb7c9e4d0f3b8a067918e420e7f05d2d9bc6
Aleksander Lantsov <lantsov.aleksander@gmail.com>
2025-02-17 21:52:11 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,47 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const (
+	CmdOk         = 0
+	InternalError = 1
+)
+
+func RunCmd(cmd []string, env Environment) (returnCode int, err error) {
+	for name, value := range env {
+		if value.NeedRemove {
+			if err := os.Unsetenv(name); err != nil {
+				return InternalError, fmt.Errorf("failed to unset environment variable %s: %w", name, err)
+			}
+		} else {
+			if err := os.Setenv(name, value.Value); err != nil {
+				return InternalError, fmt.Errorf("failed to set environment variable %s: %w", name, err)
+			}
+		}
+	}
+
+	if len(cmd) == 0 {
+		return InternalError, fmt.Errorf("no command specified")
+	}
+
+	toExecute := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	toExecute.Stdin = os.Stdin
+	toExecute.Stdout = os.Stdout
+	toExecute.Stderr = os.Stderr
+	toExecute.Env = os.Environ()
+
+	if err := toExecute.Run(); err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode(), nil
+		}
+		return InternalError, err
+	}
+
+	return CmdOk, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) <= 2 {
+		log.Fatal("Argument missing")
+	}
+
+	dir := os.Args[1]
+	customEnv, err := ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Error processing dir '%s': %s", dir, err)
+	}
+
+	code, err := RunCmd(os.Args[2:], customEnv)
+	if err != nil {
+		log.Fatalf("Error running command: %s", err)
+	}
+
+	os.Exit(code)
 }
